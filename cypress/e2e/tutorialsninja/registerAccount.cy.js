42907089466300392569f3d7742818fe8955715e
bunduca <bunductesteaza@gmail.com>
2024-02-23 11:16:57 +0200

    -added logic to generate unique email, password and phone for register tests; -updated user details fixture file;

--- cypress/e2e/tutorialsninja/registerAccount.cy.js
+++ cypress/e2e/tutorialsninja/registerAccount.cy.js
@@ -1,11 +1,19 @@
 /// <reference types="cypress" />
 
 const { baseUrl } = Cypress.config();
+const uniqueEmailTag = () => Math.floor(Math.random() * 10000000);
+const uniquePasswordTag = () => Math.floor(Math.random() * 100);
+const uniquePhone = () => Math.floor(Math.random() * 1000000000);
+
 const picture = 'samy.jpg'
 import tutorialNinjaUserDetails from "../../fixtures/tutorialNinjaUserDetails.json"
-const { email, password } = tutorialNinjaUserDetails
+const { email, password, emailPrefix, emailDomain } = tutorialNinjaUserDetails
 
 describe('Register Functionality', () => {
+
+  let uniqueEmail;
+  let uniquePassword;
+  let uniquePhoneNumber;
 
   beforeEach(() => {
     //Clear cookies and session storage (so that we are no longer logged in from previous test)
@@ -28,14 +36,23 @@ describe('Register Functionality', () => {
     //Verify redirect to the register account page
     cy.url().should('contain', '/index.php?route=account/register');
     cy.get('#content > h1').contains('Register Account').should('be.visible');
+    
+    //generate a unique email 
+    uniqueEmail = emailPrefix + uniqueEmailTag() + emailDomain
+    
+    //generate a unique password
+    uniquePassword = password + uniquePasswordTag()
+
+    //generate a unique phone number
+    uniquePhoneNumber = uniquePhone()
 
     //Enter the new account details into mandatory fields only (First Name, Last Name, email, phone, password, password confirm)
     cy.get('#input-firstname').type('Popescu');
     cy.get('#input-lastname').type('Maria');
-    cy.get('#input-email').type('bunductesteaza@gmail.com');
-    cy.get('#input-telephone').type(password);
-    cy.get('#input-password').type(password);
-    cy.get('#input-confirm').type(password);
+    cy.get('#input-email').type(uniqueEmail);
+    cy.get('#input-telephone').type(uniquePhoneNumber);
+    cy.get('#input-password').type(uniquePassword);
+    cy.get('#input-confirm').type(uniquePassword);
 
     //Select the "Privacy Policy" checkbox option
     cy.get('[type="checkbox"]').click();
